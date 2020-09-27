ca43e350adebfe5f4f147997f7f3230bbd3539aa
Anton Evdakov <aevdakov@AM.IDTCORP.NET>
2020-09-27 17:04:59 +0300

    [DB-1] fix(setup): Put out .sql script with setup for AdventureWorks2012

--- Lab1/Lab1.sql
+++ Lab1/Lab1.sql
@@ -1,13 +1,3 @@
-RESTORE FILELISTONLY FROM DISK = '/var/opt/mssql/backup/AW2012.bak'
-
-RESTORE DATABASE AdventureWorks2012 FROM DISK = '/var/opt/mssql/backup/AW2012.bak'
-WITH MOVE 'AdventureWorks2012_Data' TO '/var/opt/mssql/data/AdventureWorks2012_Data.mdf',
-    MOVE 'AdventureWorks2012_Log' TO '/var/opt/mssql/AdventureWorks2012_log.ldf'
-
--- check connection to database
-SELECT *
-FROM AdventureWorks2012.HumanResources.Department
-
 CREATE DATABASE NewDatabase;
 
 USE NewDatabase;
@@ -24,7 +14,3 @@ USE master;
 DROP DATABASE NewDatabase;
 
 RESTORE DATABASE NewDatabase FROM DISK = '/var/opt/mssql/backup/NewDatabase.bak';
-
--- come in container using powershell
--- docker exec -it mssql_db "bash"
--- /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "Password123"
