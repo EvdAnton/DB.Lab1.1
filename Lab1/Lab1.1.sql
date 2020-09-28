964dae14f8437f52cb1e9db6c28d3779065a2594
Anton Evdakov <aevdakov@AM.IDTCORP.NET>
2020-09-28 12:43:20 +0300

    [DB-1] feat(report): Add report for 1 lab

--- Lab1/Lab1.1.sql
+++ Lab1/Lab1.1.sql
@@ -1,16 +1,24 @@
 CREATE DATABASE NewDatabase;
 
 USE NewDatabase;
+GO
 
 CREATE SCHEMA sales;
+GO
+
 CREATE SCHEMA person;
+GO
 
 CREATE TABLE sales.Orders (OrderNum INT NULL);
+GO
 
 BACKUP DATABASE NewDatabase TO DISK='/var/opt/mssql/backup/NewDatabase.bak';
+GO
 
 USE master;
 
 DROP DATABASE NewDatabase;
+GO
 
 RESTORE DATABASE NewDatabase FROM DISK = '/var/opt/mssql/backup/NewDatabase.bak';
+GO
